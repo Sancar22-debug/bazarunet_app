f329b62652d3a6c5230357c9fcbca63bee6e0c11
Sanzhar Ilichbekov <sancarilichbekov@gmail.com>
2026-05-05 23:01:34 +0600

    Create currency and date formatting utilities

---
+++ src/utils/currency.js
@@ -0,0 +1,11 @@
+export const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+    }).format(amount);
+};
+
+export const formatNumber = (num) => {
+    return new Intl.NumberFormat('en-US').format(num);
+};
---
+++ src/utils/date.js
@@ -0,0 +1,19 @@
+export const formatDate = (date) => {
+    const d = typeof date === 'string' ? new Date(date) : date;
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+    }).format(d);
+};
+
+export const formatDateTime = (date) => {
+    const d = typeof date === 'string' ? new Date(date) : date;
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    }).format(d);
+};
